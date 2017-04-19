723436462ee283ad8afa0b2e62ac80df987f3d8e
ebabiche <ebabiche@ryerson.ca>
2017-04-19 07:38:17 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,31 @@
+
+
+library(shiny)
+
+# Define UI for dataset viewer application
+shinyUI(fluidPage(
+  
+    # Ryerson CKME136 Winter 2017 - SwiftKey Capstone Project.
+    titlePanel("Ryerson CKME136 Winter 2017 - SwiftKey Capstone Project"),
+  
+    sidebarLayout(
+        sidebarPanel(
+            textInput("obs", "Please type begining of your word here:"),
+            
+            helpText("This tool will use this statement to predict the next word."),
+            
+            submitButton("Ryerson CKME136 Winter 2017 - SwiftKey Capstone Project")
+        ),
+      
+      mainPanel(
+          h3("Predicted Word:"),
+          div(textOutput("BestGuess"), style = "color:blue"),
+          br(),
+          h3("Other Likely Words"),
+          tableOutput("view"),
+          
+          h5(tags$a(href="", "RPubs presentation") ),
+          h5(tags$a(href="https://github.com/ebabiche/SwiftKeyCapstoneProject", "GitHub repository") )
+    )
+  )
+))
